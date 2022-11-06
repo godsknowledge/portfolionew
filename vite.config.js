ec4838cf8c6ad4ab50c36256fb313436ef1ff1f9
IONOS Deploy Now <noreply@ionos.space>
2022-11-06 14:11:29 +0000

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData:`@import "@/scss/variables.scss";`
+            }
+        }
+    },
+    plugins: [
+        vue()
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    }
+})
